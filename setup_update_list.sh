c8d590d2a2fc14c930ae964a413412408b4f13bd
Funnnik <93551635+Funnnik@users.noreply.github.com>
2025-10-28 11:13:12 +0300

    Update setup_update_list.sh

--- setup_update_list.sh
+++ setup_update_list.sh
@@ -4,4 +4,3 @@ curl -o /opt/etc/init.d/S99update_list https://raw.githubusercontent.com/Funnnik
 curl -o /opt/bin/update_list.sh https://raw.githubusercontent.com/Funnnik/scripts/refs/heads/main/update_list.sh
 chmod +x /opt/etc/init.d/S99update_list
 chmod +x /opt/bin/update_list.sh
-rm /opt/tmp/setup_update_list.sh
